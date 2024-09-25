0918ab47827b0b9ed266e1fff62dcdb2eec3acdf
daniellemckz <daniellemckzlong@gmail.com>
2024-09-25 10:57:51 -0700

    Level Updates with Freya
    added level transitions

---
+++ Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoToLevel : MonoBehaviour
+{
+    public float level;
+
+    private void OnCollisionEnter(Collision gameObjectInformation)
+    {
+        if (gameObjectInformation.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene((int)level);
+        }
+    }
+
+}
